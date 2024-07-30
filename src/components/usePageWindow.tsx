27ce08b227ab1ded07baef4a1aacb27e4179b44f
nachovigilante <nachovigilante@gmail.com>
2024-07-30 19:47:23 -0300

    fix: fix onPageChange call to not cause a warning

--- src/components/usePageWindow.tsx
+++ src/components/usePageWindow.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 
 export default function usePageWindow(
     pageCount: number,
@@ -13,16 +13,20 @@ export default function usePageWindow(
         ) => {
             if (action.type === 'INCREMENT') {
                 if (state >= pageCount - 4) return state;
-                onPageChange(state + 2);
                 return state + 2;
             } else {
                 if (state === -2) return state;
-                onPageChange(state - 2);
                 return state - 2;
             }
         },
         -2
     );
+
+    // This call cannot be in the reducer because it would cause the component to
+    // update another component in the middle of a render cycle. This is not allowed by React.
+    useEffect(() => {
+        onPageChange(pageWindowStart);
+    }, [pageWindowStart]);
 
     const incrementWindow = () => dispatch({ type: 'INCREMENT' });
     const decrementWindow = () => dispatch({ type: 'DECREMENT' });
