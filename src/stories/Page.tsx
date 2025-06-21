2cf552e14cf271da0159d74d82461e8ae6550eba
nachovigilante <nachovigilante@gmail.com>
2025-06-21 16:40:45 -0300

    Update Page.tsx

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -10,15 +10,22 @@ export const Page: React.FC = () => {
     };
 
     const pages = Array.from({ length: 10 }, (_, i) => (
-        <h1
+        <div
             key={i}
             className="page"
             style={{
                 backgroundColor: `hsl(${i * 36}, 100%, 50%)`,
             }}
         >
-            {i + 1}
-        </h1>
+            <h1>{i + 1}</h1>
+            {Array.from({ length: 10 }, (_, j) => (<div style={{
+                backgroundColor: `hsl(${j * 36}, 100%, 50%)`,
+                width: '100%',
+                height: '50px',
+                textAlign: 'center',
+                lineHeight: '50px',
+            }}>{j + 1}</div>))}
+        </div>
     ));
 
     return (
